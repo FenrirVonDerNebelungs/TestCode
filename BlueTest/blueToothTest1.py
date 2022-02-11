ef4b4d792dcab8a911dfe65590e95d2f500a5fb4
BessPtck <elizabethp@mymindsi.com>
2022-02-11 15:26:09 -0800

    bluetooth test attempts

---
+++ BlueTest/blueToothTest1.py
@@ -0,0 +1,14 @@
+import bluetooth
+
+server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+server_sock.bind(("",bluetooth.PORT_ANY))
+server_sock.listen(1)
+
+client_sock,address=server_sock.accept()
+print("Accepted conn from:",address)
+
+data = client_sock.recv(1024)
+print("recieved:",data)
+
+client_sock.close()
+server_sock.close()
